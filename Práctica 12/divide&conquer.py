d2bdbc1b538690f8b67723a79ced446257c0cd8c
Jacqueline Paola Leyva Hernández <jacquelineleyvah@gmail.com>
2025-12-02 08:12:47 -0600

    Add QuickSort implementation with example execution
    Implement QuickSort algorithm using divide and conquer approach and add execution example.

--- Práctica 12/divide&conquer.py
+++ Práctica 12/divide&conquer.py
@@ -1,1 +1,23 @@
+# QuickSort (Divide & Conquer)
+
+def quicksort(lista):
+    if len(lista) <= 1:
+        return lista  # Caso base
+    
+    pivote = lista[len(lista) // 2]
+    menores = [x for x in lista if x < pivote]
+    iguales  = [x for x in lista if x == pivote]
+    mayores  = [x for x in lista if x > pivote]
+    
+    return quicksort(menores) + iguales + quicksort(mayores)
+
+
+# Ejecución 
+import random
+
+lista = [random.randint(1, 100) for _ in range(10)]
+
+print("Lista original:", lista)
+lista_ordenada = quicksort(lista)
+print("Lista ordenada:", lista_ordenada)
 
