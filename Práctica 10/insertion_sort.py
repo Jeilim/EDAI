7733e6a71d7c804dbfa849886a59fa147801b4ab
Jacqueline Paola Leyva Hernández <jacquelineleyvah@gmail.com>
2025-12-03 22:51:59 -0600

    Implement insertion sort with print statements
    Added an insertion sort function that sorts a list and prints the original and sorted lists.

--- Práctica 10/insertion_sort.py
+++ Práctica 10/insertion_sort.py
@@ -1,1 +1,19 @@
+def insertion_sort(lista):
+    print("Lista original:", lista)
+
+    for i in range(1, len(lista)):
+        actual = lista[i]
+        j = i - 1
+
+        while j >= 0 and lista[j] > actual:
+            lista[j + 1] = lista[j]
+            j -= 1
+
+        lista[j + 1] = actual
+
+    print("Lista ordenada :", lista)
+
+# Prueba
+numeros = [12, 5, 3, 19, 1, 8]
+insertion_sort(numeros)
 
